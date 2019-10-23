402875e89738c86d3887c2f04119e26c77d75402
Arif <faizin.arief@gmail.com>
2019-10-23 17:13:24 +0700

    Fix SectionsPagerAdapter

--- kotlin/app/src/main/java/com/dicoding/picodiploma/mytablayout/SectionsPagerAdapter.kt
+++ kotlin/app/src/main/java/com/dicoding/picodiploma/mytablayout/SectionsPagerAdapter.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
-class SectionsPagerAdapter(private val mContext: Context, fm: FragmentManager) : Fra(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+class SectionsPagerAdapter(private val mContext: Context, fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     @StringRes
     private val TAB_TITLES = intArrayOf(
